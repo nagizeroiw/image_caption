86c481fd8a49b3dc02ade10186a8199f596ce919
nagizero <nagizero@foxmail.com>
2017-11-08 10:11:24 +0800

    clear.sh

--- codes/clear.sh
+++ codes/clear.sh
@@ -1,5 +1,6 @@
 rm -f ../result/plots/*.png
-rm -f ../result/output/*.txt
+rm -f ../result/output/result_*.txt
+rm -f ../result/output/inferenced_*.json
 
 # keep the *best* checkpoints undeleted
 rm -f ../result/checkpoint/training*.ckpt
